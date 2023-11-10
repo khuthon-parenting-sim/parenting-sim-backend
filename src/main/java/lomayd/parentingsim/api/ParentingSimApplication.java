a24b640093c86698598b1c74d15d1bb1679c94fb
lomayd <jinho10167@gmail.com>
2023-11-10 18:23:42 +0900

    feat: Project Initialize

---
+++ src/main/java/lomayd/parentingsim/api/ParentingSimApplication.java
@@ -0,0 +1,13 @@
+package lomayd.parentingsim.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentingSimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentingSimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lomayd/parentingsim/ParentingSimApplicationTests.java
@@ -0,0 +1,13 @@
+package lomayd.parentingsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentingSimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
